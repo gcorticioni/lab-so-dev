a28382e59f0cfd96fb2d19a9b53344ceff436b92
gcorticioni <gabriel.corticioni@estudante.iftm.edu.br>
2026-05-14 08:47:57 -0300

    Update ola.c

--- bloco2-dev/ola.c
+++ bloco2-dev/ola.c
@@ -3,3 +3,9 @@
  * Escreva este programa diretamente no vi, sem colar.
  * Instruções na ficha de atividades.
  */
+#include <stdio.h>
+
+int main(void) {
+    printf("Olá, mundo!\n");
+    return 0;
+}
